fb901fd090ca44f930f9e975c4425157abc4b5a0
Kris Bachan <krisbachan123@hotmail.com>
2021-07-08 20:01:40 -0400

    added jquery

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    'extends': 'lighthouselabs',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {},
 };
---
+++ assets/scripts/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  let temp = function () {
+    return alert("hello");
+  };
+
+  temp();
+});
