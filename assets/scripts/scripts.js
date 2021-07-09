79467bac945dee8018f10599dc0a0f8dc82e5dc2
Kris Bachan <krisbachan123@hotmail.com>
2021-07-08 20:15:59 -0400

    started drawBarChart function and adding element to DOM

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,5 +3,25 @@ $(document).ready(function () {
     return alert("hello");
   };
 
-  temp();
+  drawBarChart(
+    [1, 2, 3, 4, 5],
+    { barOptions: { width: "200px", height: "200px" } },
+    "div"
+  );
 });
+
+const drawBarChart = function (data, options, element) {
+  let chart;
+
+  if (options.barOptions) {
+    chart = createBars(options.barOptions);
+  }
+
+  $("body").html(chart);
+};
+
+const createBars = function (options, element) {
+  let newElement = $(`<${element}></${element}>`).text("hello");
+
+  return newElement;
+};
