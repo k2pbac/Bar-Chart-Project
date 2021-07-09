6282a6721d868987303d5d4270619858e822b910
Kris Bachan <krisbachan123@hotmail.com>
2021-07-09 09:08:25 -0400

    added chart designs: title, width, height

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,7 +8,12 @@ $(document).ready(function () {
       { orange: 95 },
     ],
     {
-      graphOptions: {},
+      graphOptions: {
+        height: "400px",
+        width: "400px",
+        title: "Pop Statistics 2021",
+        shadow: "medium", // small , medium , large
+      },
       barOptions: {
         spacing: "20.5px", // any form of sizing rem, em, px.
         radius: "20%", // 0 - 100%
@@ -24,14 +29,16 @@ $(document).ready(function () {
 const drawBarChart = function (data, options, element) {
   let chart;
   let barCount = data.length;
-
+  let graph = createChart(data, options.graphOptions);
   if (options.barOptions) {
-    chart = createBars(data, options.barOptions, barCount);
+    bars = createBars(data, options.barOptions, barCount);
   }
 
-  for (let item of chart) {
-    $(".here").append(item);
+  for (let item of bars) {
+    $(graph).append(item);
   }
+
+  $(".here").append(graph);
 };
 
 const createBars = function (data, options, barCount) {
@@ -42,14 +49,11 @@ const createBars = function (data, options, barCount) {
   let currentBar = 0;
   let width = data.length * 10;
 
-  // Bar value and label
-  let barValue;
-  let barLabel;
-
   //Bar Styling
   let barPosition;
   let barDesign;
   let barSize;
+  let barValue;
 
   switch (shadow) {
     case "small":
@@ -64,7 +68,6 @@ const createBars = function (data, options, barCount) {
 
   while (barCount > 0) {
     barValue = Object.values(data[currentBar]);
-    barLabel = Object.keys(data[currentBar]);
     barPosition = `display: flex; justify-content: center; align-items: ${position}; margin-right: ${spacing};`;
     barSize = `height: ${barValue}px; width: ${width}px;`;
     barDesign = `box-shadow: ${shadowDim}; border-radius: ${radius}; border: 1px solid black; background-color: ${barColor}`;
@@ -81,5 +84,42 @@ const createBars = function (data, options, barCount) {
 };
 
 const createChart = function (data, options) {
-  let graphContainer = $(`<div></div>`);
+  let { height, width, title, shadow } = options;
+  let maxValue = 0;
+  for (let obj of data) {
+    if (maxValue < Object.values(obj)) {
+    }
+  }
+
+  console.log(maxData);
+  switch (shadow) {
+    case "small":
+      shadowDim = "4px 4px 5px grey";
+      break;
+    case "medium":
+      shadowDim = "6px 6px 5px grey";
+      break;
+    case "large":
+      shadowDim = "8px 8px 5px grey";
+  }
+
+  let graphPosition = `display: flex; justify-content: center; margin: auto; align-items: flex-end;`;
+  let graphSize = `height: ${height}; width: ${width};`;
+  let graphDesign = `border: 1px solid black; padding: 55px; padding-bottom: 10px; box-shadow: ${shadowDim};`;
+
+  let styling = `${graphPosition} ${graphSize} ${graphDesign}`;
+  let titleElement = $("<h1 style='position: fixed; top: 2rem;'></h1>").text(
+    title
+  );
+
+  let leftAxis = $(
+    "<div style='display: flex-column; position:relative; bottom: 0; left: -10%;' ></div>"
+  ).text("axis here");
+  let bottomAxis = $();
+
+  let graphContainer = $(`<div style='${styling}'></div>`)
+    .prepend(leftAxis)
+    .prepend(titleElement);
+
+  return graphContainer;
 };
