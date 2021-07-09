bafe28acd304fd5bf833732533f22870dff4ba46
Kris Bachan <krisbachan123@hotmail.com>
2021-07-08 21:17:39 -0400

    added obarOptions: radius, shadow

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -2,7 +2,13 @@ $(document).ready(function () {
   drawBarChart(
     [1, 2, 3, 4, 5],
     {
-      barOptions: { width: "200px", height: "200px", spacing: "20.5px" },
+      barOptions: {
+        width: "100px",
+        height: "100px",
+        spacing: "20.5px",
+        radius: "20%",
+        shadow: "medium",
+      },
     },
     "div"
   );
@@ -23,17 +29,29 @@ const drawBarChart = function (data, options, element) {
 
 const createBars = function (options, barCount) {
   let elements = [];
-  let { height, width, spacing } = options;
+  let { height, width, spacing, radius, shadow } = options;
   let newElement;
+  let shadowDim;
+
+  switch (shadow) {
+    case "small":
+      shadowDim = "4px 4px 5px grey";
+      break;
+    case "medium":
+      shadowDim = "6px 6px 5px grey";
+      break;
+    case "large":
+      shadowDim = "8px 8px 5px grey";
+  }
 
   while (barCount > 0) {
     if (barCount > 1) {
       newElement = $(
-        `<div style='height: ${height}; width: ${width}; margin-right: ${spacing}; border: 1px solid black;'></div>`
+        `<div style='box-shadow: ${shadowDim}; border-radius: ${radius}; height: ${height}; width: ${width}; margin-right: ${spacing}; border: 1px solid black;'></div>`
       );
     } else {
       newElement = $(
-        `<div style='height: ${height}; width: ${width}; border: 1px solid black;'></div>`
+        `<div style='box-shadow: ${shadowDim}; border-radius: ${radius}; height: ${height}; width: ${width}; border: 1px solid black;'></div>`
       );
     }
     elements.push(newElement);
